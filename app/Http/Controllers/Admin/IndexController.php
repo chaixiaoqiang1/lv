777623ef68a819ab7d5ca945a13bcf0e8f57c10b
Eagle <617701021@qq.com>
2017-09-14 20:47:22 +0800

    DB类操作数据库，view  with把变量赋给页面展示

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -4,46 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Http\Request;
-//use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class IndexController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-//    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-//    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    /*public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }*/
+    // 自定义的测试控制器
     public function index(){
+        //原生sql操作数据库的方法
         $users = DB::select('select * from l_admin');
-//var_dump($users);die;
-//        $assign = compact('data');
+        //视图层分配变量的方法
         return view('admin/index/index')->with('user',$users);
-      /*  echo 'hello world';
-        echo 'admin index/index';*/
     }
 }
